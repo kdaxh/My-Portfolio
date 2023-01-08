c3dd02b0e5a3c62a05aa3f481771b5cfbf86d2cf
kilaladashina@gmail.com <kilaladashina@gmail.com>
2023-01-08 11:00:36 +0300

    first commit

---
+++ src/components/header/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.linkedin.com/in/kilala-dashina-52b837204/" className="home__social-icon" target="_blank">
+                <i class="uil uil-linkedin-alt"></i>
+            </a>
+
+            <a href="https://github.com/kdaxh/" className="home__social-icon" target="_blank">
+                <i class="uil uil-github-alt"></i>
+            </a>
+
+            <a href="https://twitter.com/kdaxh_" className="home__social-icon" target="_blank">
+                <i class="uil uil-twitter-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social;
