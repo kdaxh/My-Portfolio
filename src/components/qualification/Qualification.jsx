6fe2e16c9b877e02caf63e016a2e06a42e4ea520
kilaladashina@gmail.com <kilaladashina@gmail.com>
2023-01-10 20:51:28 +0300

    Updated (10/01/2023)

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,12 +1,179 @@
-import React from "react";
+import React, { useState } from "react";
 import './qualification.css';
 
 const Qualification = () => {
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    };
+
     return (
         <section className="qualification section">
             <h2 className="section__title">Qualification</h2>
             <span className="section__subtitle">My personal Journey</span>
-        </section>
+
+            <div className="qualification__container container">
+                <div className="qualification__tabs">
+                    <div className={toggleState === 1
+                        ? "qualification__button qualification__active button--flex"
+                        : "qualification__button button--flex"}
+                        onClick={() => toggleTab(1)}>
+                        <i className="uil uil-graduation-cap 
+                        qualification__icon"></i>
+                        Education
+                    </div>
+
+                    <div className={toggleState === 2
+                        ? "qualification__button qualification__active button--flex"
+                        : "qualification__button button--flex"}
+                        onClick={() => toggleTab(2)}>
+                        <i className="uil uil-briefcase-alt 
+                        qualification__icon"></i>
+                        Experience
+                    </div>
+                </div>
+
+                <div className="qualification__sections">
+                    <div className={toggleState === 1
+                        ? "qualification__content qualification__content-active"
+                        : "qualification__content"}>
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">MSc in Data Science</h3>
+                                <span className="qualification__subtitle">
+                                    UDSM - Tanzania</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i>
+                                    2021 - Present
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div>
+                                <h3 className="qualification__title">BEng. in Electronics
+                                    and Telecommunication Engineering</h3>
+                                <span className="qualification__subtitle">
+                                    DIT - Tanzania</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i>
+                                    2018 - 2021
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">OD in Electronics
+                                    and Telecommunication Engineering</h3>
+                                <span className="qualification__subtitle">
+                                    ATC - Tanzania</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i>
+                                    2015 - 2018
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div>
+                                <h3 className="qualification__title">Secondary education</h3>
+                                <span className="qualification__subtitle">
+                                    Green view secondary school - Tanzania</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i>
+                                    2017 - 2018
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={toggleState === 2
+                        ? "qualification__content qualification__content-active"
+                        : "qualification__content"}>
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Intern - Computer programmer</h3>
+                                <span className="qualification__subtitle">
+                                    eGA - Tanzania</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i>
+                                    May 2020 - Nov 2020
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div>
+                                <h3 className="qualification__title">Intern - Technician</h3>
+                                <span className="qualification__subtitle">
+                                    Airtel Tanzania Limited - Tanzania</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i>
+                                    Apr to Nov (2016, 2017 & 2019)
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Intern - Data enumerator</h3>
+                                <span className="qualification__subtitle">
+                                    PCI - Tanzania</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i>
+                                    May 2018 - Jan 2019
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section >
+
+
     );
 };
 
